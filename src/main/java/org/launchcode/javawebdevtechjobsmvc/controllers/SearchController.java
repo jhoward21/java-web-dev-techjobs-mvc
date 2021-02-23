190ba0ded39f1096a0eea4b49f1e9ffac4ac468c
Jermell <afootball.life26@gmail.com>
2021-02-23 10:51:58 -0600

    Update 1

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,8 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+import static org.launchcode.javawebdevtechjobsmvc.models.JobData.findAll;
 
 /**
  * Created by LaunchCode
@@ -25,4 +27,23 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping("/search/results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String columns){
+        ArrayList<Job> jobs;
+        if(searchTerm.toLowerCase().equals("all") || searchTerm.equals(" ")){
+            jobs = JobData.findAll();
+            model.addAttribute("searchTerm", searchTerm);
+            return "results";
+
+        } else {
+            jobs = JobData.findByColumnAndValue(columns, searchTerm);
+            model.addAttribute("column", columns);
+
+        }
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        return "search";
+
+    }
 }
